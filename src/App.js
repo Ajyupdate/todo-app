e3e1ce82595fbf55a1fcc87d5756d9b9b189b55a
Ajibade <ajibadeemmanuel58@gmail.com>
2021-06-12 15:54:10 +0100

    todo app

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem'
+import todosData from "./todosData";
+
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      todos : todosData
+
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(id){
+    this.setState(prevstate =>{
+      const updatedTodos = prevstate.todos.map(todo =>{
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return{
+        todos: updatedTodos
+      }
+    })
+  }
+  
+  render(){
+    const todoItems = this.state.todos.map(item => <TodoItem handleChange={this.handleChange} key={item.id} item={item}/>)
+    return(
+      <div className="todo-list">
+        
+        {todoItems}
+      </div>
+    )
+  }
+}
+
+
+export default App
---
+++ src/todosData.js
@@ -0,0 +1,28 @@
+const todosData = [
+    {
+        id : 1,
+        text: "take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "clean gecko tank",
+        completed: true
+    },
+    {
+        id: 4,
+        text: "Moe lawn",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Catch up on Arrested Development",
+        completed: true
+    },
+]
+export default todosData
